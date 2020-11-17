c187a1cbeb53b494cee948db18603ac4b6120843
Buddy1875 <buddy1875@hotmail.com>
2020-11-17 15:38:43 +0700

    add datetime success

--- src/app/display-twitty/display-twitty.component.ts
+++ src/app/display-twitty/display-twitty.component.ts
@@ -11,25 +11,30 @@ export class DisplayTwittyComponent implements OnInit {
   constructor() {}
 
   @Input() tweet: Tweet;
-  ngOnInit() {}
+  ngOnInit() {
+    this.tweet = {
+      ...this.tweet,
+      date: this.timeAgo(this.tweet.date.toDate())
+    }
+  }
 
   timeAgo(val: Date) {
     const now = new Date();
     const diff = Math.abs(now.getTime() - val.getTime());
-    const diffDay = Math.ceil(diff / (100 * 3600 * 24));
+    const diffDay = Math.ceil(diff / (1000 * 3600 * 24));
     const diffHouse = Math.ceil(diff / (1000 * 3600));
-    const diffMinute = Math.ceil(diff / (100 * 60));
+    const diffMinute = Math.ceil(diff / (1000 * 60));
     const diffSecond = Math.ceil(diff / 1000);
 
     if (diffSecond < 60) {
       return "Juss now";
     }
 
-    if (diffMinute) {
+    if (diffMinute < 60) {
       return `${diffMinute} mintue(s) ago`;
     }
 
-    if (diffHouse < 60) {
+    if (diffHouse < 24) {
       return `${diffHouse} Hour(s) ago`;
     }
     return `${diffDay} day(s) ago`;
