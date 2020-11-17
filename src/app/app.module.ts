093e908834ebc8637ee883b985f04d890a0da18d
Buddy1875 <buddy1875@hotmail.com>
2020-11-17 11:40:04 +0700

    add enviroment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';;
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { AngularFireModule } from "@angular/fire";
+import { enviroment } from "./enviroment";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { AddTwittyComponent } from './add-twitty/add-twitty.component';
-import { HomeComponent } from './home/home.component';
-import { TimeLineComponent } from './time-line/time-line.component';
-import { DisplayTwittyComponent } from './display-twitty/display-twitty.component';
-
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { AddTwittyComponent } from "./add-twitty/add-twitty.component";
+import { HomeComponent } from "./home/home.component";
+import { TimeLineComponent } from "./time-line/time-line.component";
+import { DisplayTwittyComponent } from "./display-twitty/display-twitty.component";
 
 @NgModule({
-  imports:      [
-     BrowserModule,
-      FormsModule ,
-      RouterModule.forRoot([
-        {path: "time-line", component: TimeLineComponent},
-        {path: "add-twitty", component: AddTwittyComponent}
-      ]),
-      ReactiveFormsModule
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: "time-line", component: TimeLineComponent },
+      { path: "add-twitty", component: AddTwittyComponent }
+    ]),
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(enviroment.firebaseConfig)
   ],
   declarations: [
-    AppComponent, 
+    AppComponent,
     HelloComponent,
     AddTwittyComponent,
     HomeComponent,
     TimeLineComponent,
-    DisplayTwittyComponent ],
-  bootstrap:    [ AppComponent ]
+    DisplayTwittyComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/enviroment.ts
@@ -0,0 +1,12 @@
+export const enviroment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBJ1geCWa_ywjBNABzBJ82X7kUCmPj9rqU",
+    authDomain: "twitty-project-b7326.firebaseapp.com",
+    databaseURL: "https://twitty-project-b7326.firebaseio.com",
+    projectId: "twitty-project-b7326",
+    storageBucket: "twitty-project-b7326.appspot.com",
+    messagingSenderId: "12695915555",
+    appId: "1:12695915555:web:97268ce456c72f8e3e3596",
+    measurementId: "G-ND6TYCJERW"
+  }
+};
